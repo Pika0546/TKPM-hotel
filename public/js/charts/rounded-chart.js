1a20580935f2bb3de0d1122323c7f14b9ce861d8
khoale <kthedestroy@gmail.com>
2022-04-30 14:20:54 +0700

    update public/js to have 2 piecharts

--- public/js/charts/rounded-chart.js
+++ public/js/charts/rounded-chart.js
@@ -4,8 +4,29 @@
 	 /*----------------------------------------*/
 	/*  1.  pie Chart
 	/*----------------------------------------*/
-	var ctx = document.getElementById("piechart");
-	var piechart = new Chart(ctx, {
+	var ctx1 = document.getElementById("piechart1");
+	var piechart1 = new Chart(ctx1, {
+		type: 'pie',
+		data: {
+			labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+			datasets: [{
+				label: 'pie Chart',
+                backgroundColor: [
+					'#303030',
+					'#933EC5',
+					'#65b12d',
+					'#D80027',
+					'#006DF0'
+				],
+				data: [10, 20, 30, 40, 60]
+            }]
+		},
+		options: {
+			responsive: true
+		}
+	});
+	var ctx2 = document.getElementById("piechart2");
+	var piechart2 = new Chart(ctx2, {
 		type: 'pie',
 		data: {
 			labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
