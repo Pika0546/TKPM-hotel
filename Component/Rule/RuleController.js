f92bf8a8cb55223d3004e066ab4f9f66f6d0d954
khoale <kthedestroy@gmail.com>
2022-05-09 23:25:55 +0700

    Frontend: rule, admin

--- Component/Rule/RuleController.js
+++ Component/Rule/RuleController.js
@@ -1,6 +1,25 @@
+const createError = require('http-errors');
+
+const RuleService = require('./RuleService');
+const PageUtil = require('../../utils/page');
+const ObjectUtil = require('../../utils/object');
+
+const maximumPagination = 5;
+const limit = 5;
+
+let currentGuestTypePage = 1;
+let totalGuestTypePage = 1;
+let totalGuestType = 0;
+
+let currentRoomTypePage = 1;
+let totalRoomTypePage = 1;
+let totalRoomType = 0;
+
 class RuleController{
     getRulePage = async (req, res, next) => {
+        
         res.render('rule/index');
+        
     } 
 }
 
