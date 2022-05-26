6bb538986002164b809be0b91aeea1c9ef775ce4
khoale <kthedestroy@gmail.com>
2022-05-26 09:01:54 +0700

    BE: delete room rent API; FE: fix css, js link for 404, 500 page

--- Component/RoomRent/RoomRentController.js
+++ Component/RoomRent/RoomRentController.js
@@ -10,6 +10,44 @@ class RoomRentController{
     getDetailRoomRent = async (req, res, next) => {
         res.render('roomrent/edit');
     }
+    deleteRoomRentAPI = async (req, res, next) => {
+        try {
+            const {id} = req.params;
+            if(!id){
+                res.status(200).json({
+                    success: false,
+                    message: "Đầu vào không hợp lệ!"
+                });
+                return;
+            }
+            const resData = await RoomRentService.findRoomRentById(id);
+            if(!resData){
+                res.status(200).json({
+                    success: false,
+                    message: "Phiếu thuê không tồn tại!"
+                });
+                return;
+            }
+            if(!resData.billId){
+                res.status(200).json({
+                    success: false,
+                    message: "Không thể xóa: Phiếu thuê này chưa được thanh toán!"
+                });
+                return;
+            }
+            const resData2 = await RoomRentService.deleteRoomRentById(id);
+            res.status(200).json({
+                success: true,
+                roomRent: resData2,
+                message: 'Xóa thành công!'
+            })
+            return;
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
 }
 
 module.exports = new RoomRentController();
--- Component/RoomRent/RoomRentService.js
+++ Component/RoomRent/RoomRentService.js
@@ -1,5 +1,21 @@
-class RoomRentService{
+const { models } = require("../../models");
 
+class RoomRentService{
+    findRoomRentById = async (id) => {
+        return models.roomrent.findOne({
+            raw:true,
+            where:{
+                id:id
+            }
+        })
+    }
+    deleteRoomRentById = async (id) => {
+        return models.roomrent.destroy({
+            where: {
+                id: id
+            },
+        });
+    }
 }
 
 module.exports = new RoomRentService();
