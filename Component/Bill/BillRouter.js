b93e6ae9669af3b4fd57e2b4d228a33537db4697
LUU HUYNH NGUYEN THI <luuhuynh.tkp@gmail.com>
2022-06-14 16:07:10 +0700

    list bill /find by guest name, date/

--- Component/Bill/BillRouter.js
+++ Component/Bill/BillRouter.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const BillController = require('./BillController');
 
 router.get('/', BillController.getBillList);
+router.get('/api', BillController.getBillListApi);
 router.get('/add', BillController.getAddBill);
 router.get('/detail', BillController.getDetailBill);
+router.get('/detail/:id', BillController.getDetailBill);
 
 module.exports = router;
--- Component/Bill/BillService.js
+++ Component/Bill/BillService.js
@@ -1,5 +1,65 @@
-class BillService{
+const { Op } = require("sequelize");
 
+const { models } = require("../../models");
+const guest = require('../../models/guest');
+
+class BillService{
+    getBillList = async (limit, page, guestName, rentDateFrom, rentDateTo, valueFrom, valueTo) => {
+        console.log(rentDateFrom);
+        console.log(rentDateTo);
+        return models.bill.findAll({
+            raw: true,
+            offset: (page-1)*limit,
+            limit: limit,
+            where:{
+                ...(rentDateFrom && rentDateTo && {createdAt: {[Op.between]: [rentDateFrom, rentDateTo]}}),
+                //...(valueFrom && valueTo && {value: {[Op.between]: [valueFrom, valueTo]}})
+            },
+            include:[
+                {
+                    model: models.guest,
+                    where:{
+                        ...(guestName && guestName.length && {fullname: {[Op.substring]:guestName}})
+                    },
+                    require: true
+                },
+            ]
+        })
+    }
+    countAllBill = async (guestName, rentDateFrom, rentDateTo, valueFrom, valueTo) => {
+        return models.bill.count({
+            where:{
+                ...(rentDateFrom && rentDateTo && rentDateFrom.length && rentDateTo.length && {createdAt: {[Op.between]: [rentDateFrom, rentDateTo]}}),
+                //...(valueFrom && valueTo.length && {value: {[Op.between]: [valueFrom, valueTo]}}),
+            },
+            include:[
+                {
+                    model: models.guest,
+                    where:{
+                        ...(guestName && guestName.length && {fullname: {[Op.substring]:guestName}})
+                    },
+                    require: true
+                },
+            ]
+        })
+    }
+
+    findBillById = async (id) => {
+        return models.bill.findOne({
+            raw: true,
+            where: {
+                id:id
+            }
+        })
+    }
+
+    deleteBillById = async (id) => {
+        return models.bill.destroy({
+            where:{
+                id:id
+            }
+        })
+    }
 }
 
 module.exports = new BillService();
